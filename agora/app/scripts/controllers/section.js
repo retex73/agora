4f88d794ef9ad0b4a677c8d608e9938011d83390
pwilliams2 <pwilliams2@gmc-uk.org>
2015-03-02 17:27:52 +0000

    updated sections page

--- agora/app/scripts/controllers/section.js
+++ agora/app/scripts/controllers/section.js
@@ -8,7 +8,7 @@
  * Controller of the agoraApp
  */
 angular.module('agoraApp')
-  .controller('SectionCtrl', function($scope, $route, $parse, $location) {
+  .controller('SectionCtrl', function($scope, $route, $parse, $location, $document) {
 
     // console.log($route.current.params); 
     $scope.sectionText = "Hover over the left icons for more info"; 
@@ -25,22 +25,65 @@ angular.module('agoraApp')
       
     }; 
 
+    $scope.sectionImages = {
+      'Stats' : 'stats.png', 
+      'UK maps': 'uk.png', 
+      'World maps': 'world.png'
+    }; 
+
     var colours = ['#2a3f61','#2ca7bf','#5a2a80','#b05898','#7c418c','#7a71ad','#4275b3']; 
 
     $scope.sectionDescriptions = agora.reports.getSectionDescriptions(pagesObj, Object.keys($scope.pages));
 
 
+    function hex2rgba(hex, opacity){
+        //extract the two hexadecimal digits for each color
+        var patt = /^#([\da-fA-F]{2})([\da-fA-F]{2})([\da-fA-F]{2})$/;
+        var matches = patt.exec(hex);
 
-    // console.log($scope.sectionDescriptions); 
+        //convert them to decimal
+        var r = parseInt(matches[1], 16);
+        var g = parseInt(matches[2], 16);
+        var b = parseInt(matches[3], 16);
+
+        //create rgba string
+        var rgba = "rgba(" + r + "," + g + "," + b + "," + opacity + ")";
+
+        //return rgba colour
+        return rgba;
+    }
+
+    var getSectionColour = function() {
+      var colours = ['#2a3f61','#2ca7bf','#5a2a80','#b05898','#7c418c','#7a71ad','#4275b3']; 
+      var colour = 0; 
+      var retColour; 
+      $.each(groups, function(key, val){
+      
+        if(key == $scope.sectionName) {
+          // console.log(key + ' is eq to ' + $scope.sectionName); 
+          // console.log(colours[colour]); 
+          retColour = colours[colour]; 
+        }
+        colour++; 
+      }); 
+
+      return retColour; 
+    }; 
+
+
+    $scope.sectionColour = getSectionColour(); 
+
+    // console.log('Section Colour: ' + $scope.sectionColour); 
     
+    var setSectioncolour = function() {   
+      $(".report-groups ul li").css('background', $scope.sectionColour);  
+      $("#section-info").css('background', hex2rgba($scope.sectionColour, 0.9)); 
 
-    var i = 0; 
-
-    $.each(groups, function(key, val){
-      console.log(key); 
-    }); 
+    }; 
     
-
+    setTimeout(function(){
+      setSectioncolour(); 
+    }, 10); 
 
 
     $scope.gotoViz = function(url) {
@@ -48,3 +91,4 @@ angular.module('agoraApp')
     }
 
   });
+
