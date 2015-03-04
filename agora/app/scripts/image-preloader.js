12b2c0c97fae68559c07a6fcd7fdea9e9283be3a
pwilliams2 <pwilliams2@gmc-uk.org>
2015-03-04 12:10:48 +0000

    added themr

---
+++ agora/app/scripts/image-preloader.js
@@ -0,0 +1,24 @@
+var agora = window.agora || {};
+
+(function() {
+	agora.themr = {
+
+
+		setCurrentState: function(section) {						
+			return agora.reports.getSectionTheme(section); 
+		}, 
+
+		preloader: function(){
+
+		}, 
+
+
+
+
+		
+
+
+
+
+	}
+})();
