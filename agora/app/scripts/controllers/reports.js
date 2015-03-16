335aaf7d975d42fadea508af06233eb6063d8f42
pwilliams2 <pwilliams2@gmc-uk.org>
2015-03-16 15:40:38 +0000

    updated reports section

--- agora/app/scripts/controllers/reports.js
+++ agora/app/scripts/controllers/reports.js
@@ -14,7 +14,9 @@ angular.module('agoraApp')
     $scope.bgClass = ""; 
 
     $scope.defaultText = "Please choose the data report you would like to explore"; 
-    
+    $scope.gotoReports = function(link) {            
+            window.location.href = link; 
+    }; 
 
     $scope.getDescription = function(desc, key) {
     	
@@ -30,23 +32,54 @@ angular.module('agoraApp')
 
         
 
+
         
-    	var colours = ['#2a3f61','#2ca7bf','#5a2a80','#b05898','#7c418c','#7a71ad','#4275b3']; 
+    	// var colours = ['#2a3f61','#2ca7bf','#5a2a80','#b05898','#7c418c','#7a71ad','#4275b3']; 
+        var colours = ['42,63,97','44,167,191','90,42,128','176,88,152','124,65,140','122,113,173','66,117,179']; 
 
 
     	$(".report-group-info-text").css({
-    		'background': colours[key], 
+    		'background': 'rgba(' + colours[key] + ', 0.4)', 
     		'color': '#fff'
     	}); 
 
-
-
-
     }; 
-
     
   });
 
 
+$(document.body).on('click', '.report-group-link-box', function(e){
+    e.preventDefault(); 
+    var top = $(this).position().top; 
+    top = top + 4; 
+    var bgCol = $(this).closest('li').css("background-color");
 
+    // get numbers
+    var n = bgCol.replace(/[^\d,]/g, '').split(',');
+    
+
+
+    
+    
+
+    // $('.bubble').offset({top: top});
+    $('.bubble').show(); 
+    
+
+    $('.bubble').stop(); 
+     
+    $('.bubble').animate({
+        'top': top, 
+    }); 
+
+    var rgba = n.toString() + ", 0.4"; 
+    var bgProp = "50px solid rgba(" + rgba + ")"; 
+    
+    
+    $('.bubble').css("border-right", bgProp); 
+    $('.get-started button').css('background', bgCol);
+    $('.section-info-rollover a').css('color', bgCol); 
+
+
+}); 
 
